3b682c60eaecff9d09f09568371fed491b50c83d
Peter Vasilyev <p.vasilyev@inbox.ru>
2019-03-12 21:47:39 +0300

    #5 Add interactive object
    - Add InteractiveObjectController sricpt.
    - Add example of button and door.

---
+++ Assets/Scripts/DemoScene/DemoDoorController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemoDoorController : MonoBehaviour
+{
+    public float openTime;
+    public float openHeight;
+
+    private bool isMoving;
+    private float moveTimer;
+
+    private Vector3 curPos;
+    private Vector3 targetPos;
+
+    private void Start()
+    {
+        curPos = transform.position;
+        targetPos = transform.position + new Vector3(0, openHeight, 0);
+    }
+
+    public void Interact()
+    {
+        if (!isMoving)
+        {
+            isMoving = true;
+        }
+    }
+
+    void Update()
+    {
+        if (isMoving)
+        {
+            moveTimer += Time.deltaTime;
+
+            if (moveTimer >= openTime)
+            {
+                moveTimer = 0;
+
+                isMoving = false;
+
+                transform.position = targetPos;
+                Vector3 tmp = targetPos;
+                targetPos = curPos;
+                curPos = tmp;
+            }
+
+            transform.position = Vector3.Lerp(curPos, targetPos, moveTimer / openTime);
+        }
+    }
+}

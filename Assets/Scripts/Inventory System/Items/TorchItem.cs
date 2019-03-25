54f5dd6735610058d3e2aa86e43de69c3c520407
ik6cgsg <kozlovilya@bk.ru>
2019-03-26 01:51:36 +0300

    #67 Add torch item
     * Add torch prefab
     * Add torch material
     * Add fire particle system
     * Add empty game object 'Usables' as player's child
     * Add folders for 'usables' in Materials, Inventory system and Prefabs

---
+++ Assets/Scripts/Inventory System/Items/TorchItem.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorchItem : IItem
+{
+    public override string Name
+    {
+        get
+        {
+            return "Tourch";
+        }
+    }
+
+    public override Texture IconTexture
+    {
+        get
+        {
+            return Texture2D.blackTexture;
+        }
+    }
+
+    public override void Use()
+    {
+        Debug.Log("I am a torch.");
+        GameObject torch = TorchUsableController.Instance;
+        bool status = torch.activeSelf; 
+        torch.SetActive(!status);
+    }
+
+    public override bool IsUsable()
+    {
+        return true;
+    }
+}

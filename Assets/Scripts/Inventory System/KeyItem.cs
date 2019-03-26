d5a2c328c4c4bcf94a7dfc00a47d82b5722a83e1
OLavrik <olya_lavrik@inbox.ru>
2019-03-26 22:57:54 +0300

    #60 add key

---
+++ Assets/Scripts/Inventory System/KeyItem.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyItem : IItem
+{
+    private string name;
+    // Start is called before the first frame update
+    void Start()
+    {
+        name = "Key";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override string Name
+    {
+        get
+        {
+            return name;
+        }
+    }
+
+    public override Texture IconTexture
+    {
+        get
+        {
+            return null;
+        }
+    }
+}

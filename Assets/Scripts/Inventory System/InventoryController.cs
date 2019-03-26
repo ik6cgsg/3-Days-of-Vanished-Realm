34529d2f02112cb905b7b5b22e3051a646ef5915
Ppasha9 <pavelvan50@gmail.com>
2019-03-26 20:55:00 +0300

    #30 Inventory system
    - add first version of inventory system implementation

--- Assets/Scripts/Inventory System/InventoryController.cs
+++ Assets/Scripts/Inventory System/InventoryController.cs
@@ -1,61 +1,81 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class InventoryController: MonoBehaviour
 {
     private static int maxSize;
     private static int maxAvailableSize;
-    private static IItem[] items;
+    private static List<IItem> items;
 
     // Adding new item to array
     // Returns false if inventory is full, true otherwise
     public static bool AddItem(IItem item)
     {
-        return false;
+        if (IsFull())
+        {
+            return false;
+        }
+
+        items.Add(item);
+        return true;
     }
 
-    // Dropping item from inventory to the environment
-    public static void DropItem(int index)
+    // Removing item from inventory
+    public static void RemoveItem(int index)
     {
-
+        items.RemoveAt(index);
     }
 
     // Getting item from array by its index
     public static IItem GetItem(int index)
     {
-        return new EmptyItem();
+        return index >= 0 && index < items.Count ? items[index] : new EmptyItem();
     }
 
     // Getting item from array by its name
     public static IItem GetItem(string name)
     {
+        foreach (IItem item in items)
+        {
+            if (item.Name == name)
+            {
+                return item;
+            }
+        }
+
         return new EmptyItem();
     }
 
     // Does we have this item in our inventory
     public static bool HasItem(IItem item)
     {
-        return false;
+        return items.Contains(item);
     }
 
     public static bool IsFull()
     {
-        return false;
+        return items.Count == maxAvailableSize;
     }
 
     // Increasing the maximum amount of items in inventory
     public static void IncreaseSize(int increaseRate)
     {
-
+        maxSize += increaseRate;
     }
 
     // Increasing the maximum amount of available items in inventory
     public static void IncreaseAvailableSize(int increaseRate)
     {
-
+        maxAvailableSize += increaseRate;
     }
 
     public void Start()
     {
+        // --- For test ---
+        maxSize = 4;
+        maxAvailableSize = 1;
+        // ------
 
+        items = new List<IItem>();
     }
 }

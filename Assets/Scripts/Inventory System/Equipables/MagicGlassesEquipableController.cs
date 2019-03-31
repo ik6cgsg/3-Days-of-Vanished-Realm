473f2daf21f16e146d7cc680b6236d76e4f720f2
Ppasha9 <pavelvan50@gmail.com>
2019-03-31 22:49:43 +0300

    #88 Add magic glasses
    - Add MagicGlasses prefab
    - Add this prefab to test scene
    - Create test materials for MagicGlasses
    - Rename TorchUsable.mat to TorchEquipable.mat

---
+++ Assets/Scripts/Inventory System/Equipables/MagicGlassesEquipableController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MagicGlassesEquipableController : MonoBehaviour
+{
+    public static UnityEvent magicGlassesOn;
+    public static UnityEvent magicGlassesOff;
+
+    public Transform mainCamera;
+    private Material equippedMaterial;
+
+    public void Start()
+    {
+        magicGlassesOn = new UnityEvent();
+        magicGlassesOff = new UnityEvent();
+
+        gameObject.SetActive(false);
+    }
+
+    public void OnEnable()
+    {
+        magicGlassesOn.Invoke();
+    }
+
+    public void OnDisable()
+    {
+        magicGlassesOff.Invoke();
+    }
+
+    public void LateUpdate()
+    {
+        transform.position = mainCamera.position;
+    }
+}
---
+++ Assets/Scripts/Inventory System/Items/MagicGlassesItem.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicGlassesItem : IItem
+{
+    public override string Name
+    {
+        get
+        {
+            return "MagicGlasses";
+        }
+    }
+
+    public override EquipmentController.EquipableItem EquipableItem
+    {
+        get
+        {
+            return EquipmentController.EquipableItem.MAGIC_GLASSES;
+        }
+    }
+
+    private void OnEnable()
+    {
+        Texture iconTex = Resources.Load<Texture>("Textures/Items/MagicGlassesTex");
+        iconEquippedTexture = iconTex;
+        iconUnequippedTexture = iconTex;
+        IconTexture = iconUnequippedTexture;
+    }
+
+    public override bool IsEquipable()
+    {
+        return true;
+    }
+}

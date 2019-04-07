c046c1d90fe920ee73d028905ffcb250fe581afd
ik6cgsg <kozlovilya@bk.ru>
2019-04-07 22:36:56 +0300

    #109 Add burnable item controller
    - Add half transparent burnable sphere
    - Add burnable item controller for it

---
+++ Assets/Scripts/InteractiveObject/BurnableItemController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BurnableItemController : IInteractiveObject
+{
+    public GameObject burnableObject;
+    public float burnTime;
+    public bool IsOnFire
+    {
+        get;
+        private set;
+    }
+
+    private GameObject fire;
+    private float fireStartTime;
+
+    void Start()
+    {
+        IsOnFire = false;
+        Transform[] children = GetComponentsInChildren<Transform>();
+        foreach (Transform child in children)
+        {
+            if (child.name.Equals("Fire"))
+            {
+                fire = child.gameObject;
+                break;
+            }
+        }
+        fire.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (IsOnFire && Time.time - fireStartTime >= burnTime)
+        {
+            Destroy(burnableObject);
+            IsOnFire = false;
+        }
+    }
+
+    public override void Interact()
+    {
+        if (EquipmentController.CurrentItem == EquipmentController.EquipableItem.TORCH)
+        {
+            IsOnFire = true;
+            fire.SetActive(true);
+            fireStartTime = Time.time;
+        }
+    }
+
+    public override bool CanInteract()
+    {
+        if (EquipmentController.CurrentItem == EquipmentController.EquipableItem.TORCH)
+        {
+            return true;
+        }
+        return false;
+    }
+}

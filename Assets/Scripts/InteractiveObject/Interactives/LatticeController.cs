0327bc52df7382ad091fc77b5e50b67578c2319d
Peter Vasilyev <p.vasilyev@inbox.ru>
2019-04-10 02:01:50 +0300

    #91 Add lattice
    - Move prefab and script to correct folder

---
+++ Assets/Scripts/InteractiveObject/Interactives/LatticeController.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+
+public class LatticeController : IInteractiveObject
+{
+    public float openTime;
+    public float openHeight;
+    public FloorController barredFloorController;
+
+    private bool isMoving;
+    private float moveTimer;
+    private bool isClosed;
+
+    private Vector3 curPos;
+    private Vector3 targetPos;
+
+    void Start()
+    {
+        curPos = transform.position;
+        targetPos = transform.position + new Vector3(0, openHeight, 0);
+        isClosed = true;
+    }
+
+    private void Awake()
+    {
+        barredFloorController.enabled = false;
+    }
+
+    void Update()
+    {
+        if (isMoving)
+        {
+            moveTimer += Time.deltaTime;
+
+            if (moveTimer >= openTime)
+            {
+                moveTimer = 0;
+
+                isMoving = false;
+
+                transform.position = targetPos;
+                Vector3 tmp = targetPos;
+                targetPos = curPos;
+                curPos = tmp;
+
+                barredFloorController.enabled = true;
+            }
+
+            transform.position = Vector3.Lerp(curPos, targetPos, moveTimer / openTime);
+        }
+    }
+
+    public override void Interact()
+    {
+        isMoving = true;
+        isClosed = false;
+    }
+
+    public override bool CanInteract()
+    {
+        return isClosed;
+    }
+}

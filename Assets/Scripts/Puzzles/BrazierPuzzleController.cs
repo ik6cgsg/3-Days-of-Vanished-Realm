cba8eb4a6ce57696d6a2ac266122a6661c7a016e
ik6cgsg <kozlovilya@bk.ru>
2019-04-14 19:26:45 +0300

    #145 Add brazier puzzle
    - Add new room with magnificent fire puzzle

---
+++ Assets/Scripts/Puzzles/BrazierPuzzleController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrazierPuzzleController : MonoBehaviour
+{
+    public TorchInteractiveController[] braziers;
+    public GameObject[] lights;
+    public IInteractiveObject platform;
+
+    public bool Solved
+    {
+        private set;
+        get;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (GameObject l in lights)
+        {
+            l.SetActive(false);
+        }
+        Solved = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!Solved && AllLit())
+        {
+            platform.Interact();
+            Destroy(platform.GetComponent("FloorController"));
+            Solved = true;
+        }
+    }
+
+    private bool AllLit()
+    {
+        bool res = true;
+        for (int i = 0; i < braziers.Length; i++)
+        {
+            if (!braziers[i].IsLit)
+            {
+                res = false;
+                continue;
+            }
+            lights[i].SetActive(true);
+        }
+        return res;
+    }
+}

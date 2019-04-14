179f4806a3709188ca2ddbe7be6b17e4e78fa5cc
ik6cgsg <kozlovilya@bk.ru>
2019-04-14 14:53:53 +0300

    #156 Add sound to interactive object
    - Add example "Detector" with sound

--- Assets/Scripts/InteractiveObject/IInteractiveObject.cs
+++ Assets/Scripts/InteractiveObject/IInteractiveObject.cs
@@ -4,6 +4,23 @@ using UnityEngine;
 
 public abstract class IInteractiveObject : MonoBehaviour
 {
+    public virtual AudioSource Sound
+    {
+        get
+        {
+            return null;
+        }
+    }
+
+    public void InteractGlobal()
+    {
+        if (Sound != null)
+        {
+            Sound.Play();
+        }
+        Interact();
+    }
+
     public abstract void Interact();
 
     public virtual bool CanInteract()

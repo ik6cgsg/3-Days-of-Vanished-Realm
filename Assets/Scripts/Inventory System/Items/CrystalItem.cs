397e5452a89a28221b3862f243f6809d3f0e1708
Peter Vasilyev <p.vasilyev@inbox.ru>
2019-04-20 20:17:12 +0300

    #158 Add pickupable crystal icon

--- Assets/Scripts/Inventory System/Items/CrystalItem.cs
+++ Assets/Scripts/Inventory System/Items/CrystalItem.cs
@@ -1,4 +1,6 @@
-﻿public class CrystalItem : IItem
+﻿using UnityEngine;
+
+public class CrystalItem : IItem
 {
     public override string Name
     {
@@ -6,5 +8,12 @@
         {
             return "Crystal";
         }
+    }
+
+    private void OnEnable()
+    {
+        iconEquippedTexture = Resources.Load<Texture>("Textures/GUI/GuiInventory/Items/GuiCrystalIcon");
+        iconUnequippedTexture = Resources.Load<Texture>("Textures/GUI/GuiInventory/Items/GuiCrystalIcon");
+        IconTexture = iconUnequippedTexture;
     }
 }

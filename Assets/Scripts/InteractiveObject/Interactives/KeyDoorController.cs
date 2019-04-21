7f600012019918c25c1c98fa60a69502fccd1e61
OLavrik <olya_lavrik@inbox.ru>
2019-04-22 02:21:27 +0300

    #154 Add door opening sound

--- Assets/Scripts/InteractiveObject/Interactives/KeyDoorController.cs
+++ Assets/Scripts/InteractiveObject/Interactives/KeyDoorController.cs
@@ -6,12 +6,21 @@ public class KeyDoorController : IInteractiveObject
 {
     public float openTime;
     public float openHeight;
+    public AudioSource soundRef;
 
     private bool isMoving;
     private float moveTimer;
 
     private Vector3 curPos;
     private Vector3 targetPos;
+
+    public override AudioSource Sound
+    {
+        get
+        {
+            return soundRef;
+        }
+    }
 
     // Start is called before the first frame update
     void Start()

4d17c2fc14a2a08fe46d9bba72c262a1a4498fa7
ik6cgsg <kozlovilya@bk.ru>
2019-05-07 18:47:58 +0300

    #197 Add portal prefab

--- Assets/Scripts/Portal/PortalCamera.cs
+++ Assets/Scripts/Portal/PortalCamera.cs
@@ -1,30 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VR;
 
 public class PortalCamera : MonoBehaviour
 {
     public Transform playerCamera;
     public Transform portalIn;
     public Transform portalOut;
-    public float offsetAngle;
+    public Material cameraMaterial;
+
+    private Vector3 forward;
 
     void Start()
     {
-        //offsetAngle = -Vector3.Angle(portalIn.up, -playerCamera.forward);
+        RenderTexture rt = new RenderTexture(Screen.width, Screen.height, 24);
+        GetComponent<Camera>().targetTexture = rt;
+        cameraMaterial.mainTexture = rt;
     }
 
-    void LateUpdate()
+    void Update()
     {
+        //transform.localPosition = UnityEngine.XR.InputTracking.GetLocalPosition(UnityEngine.XR.XRNode.CenterEye);
+        //transform.localRotation = UnityEngine.XR.InputTracking.GetLocalRotation(UnityEngine.XR.XRNode.CenterEye);
         Vector3 playerOffsetFromPortal = playerCamera.position - portalIn.position;
         transform.position = portalOut.position - playerOffsetFromPortal;
         float angularDifferenceBetweenPortals = Quaternion.Angle(portalIn.rotation, portalOut.rotation);
         Quaternion portalRotationDifference = Quaternion.AngleAxis(angularDifferenceBetweenPortals, Vector3.up);
         Vector3 newCameraDirection = portalRotationDifference * playerCamera.forward;
-        transform.rotation = Quaternion.LookRotation(newCameraDirection, Vector3.up);
-        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x,
-                                              transform.rotation.eulerAngles.y - 180,
-                                              playerCamera.eulerAngles.z);
-                                              
+        
+        //transform.rotation = Quaternion.LookRotation(newCameraDirection, Vector3.up);
+        transform.rotation = Quaternion.Euler(playerCamera.rotation.eulerAngles.x,
+                                              playerCamera.rotation.eulerAngles.y - 180,
+                                              playerCamera.rotation.eulerAngles.z);
+        
+        //transform.localPosition = transform.localPosition + UnityEngine.XR.InputTracking.GetLocalPosition(UnityEngine.XR.XRNode.CenterEye);
     }
 }
--- Assets/Shaders/Portal/ScreenCutoutShader.shader
+++ Assets/Shaders/Portal/ScreenCutoutShader.shader
@@ -1,57 +1,63 @@
-// Upgrade NOTE: replaced 'mul(UNITY_MATRIX_MVP,*)' with 'UnityObjectToClipPos(*)'
-
 Shader "Unlit/ScreenCutoutShader"
 {
+	//UNITY_SHADER_NO_UPGRADE
+
 	Properties
 	{
-		_MainTex ("Texture", 2D) = "white" {}
+		_MainTex("Texture", 2D) = "white" {}
 	}
+
 	SubShader
 	{
-		Tags{ "Queue" = "Transparent" "IgnoreProjector" = "True" "RenderType" = "Transparent" }
+		Tags 
+		{
+			"Queue" = "Geometry"
+			"RenderType" = "Opaque" 
+		}
+		LOD 100
+
 		Lighting Off
-		Cull Back
-		ZWrite On
-		ZTest Less
-		
-		Fog{ Mode Off }
+		ZTest Always
+		Fog
+		{
+			Mode Off
+		}
+		Blend SrcAlpha OneMinusSrcAlpha
 
 		Pass
 		{
 			CGPROGRAM
-			#pragma vertex vert
-			#pragma fragment frag
-			
+			#pragma vertex Vert
+			#pragma fragment Frag
+
 			#include "UnityCG.cginc"
 
-			struct appdata
+			struct Appdata
 			{
 				float4 vertex : POSITION;
 				float2 uv : TEXCOORD0;
 			};
 
-			struct v2f
+			struct V2F
 			{
-				//float2 uv : TEXCOORD0;
-				float4 vertex : SV_POSITION;
-				float4 screenPos : TEXCOORD1;
+				float4 vertex : POSITION;
 			};
 
-			v2f vert (appdata v)
+			sampler2D _MainTex;
+			float4 _MainTex_ST;
+
+			V2F Vert(Appdata v)
 			{
-				v2f o;
-				o.vertex = UnityObjectToClipPos(v.vertex);
-				o.screenPos = ComputeScreenPos(o.vertex);
+				V2F o;
+				//o.vertex = UnityObjectToClipPos(v.vertex);
+				//o.vertex = mul(UNITY_MATRIX_VP, mul(unity_ObjectToWorld, float4(v.vertex.xyz, 1.0)));
+				o.vertex = mul(UNITY_MATRIX_MVP, float4(v.vertex.xyz, 1.0));
 				return o;
 			}
-			
-			sampler2D _MainTex;
 
-			fixed4 frag (v2f i) : SV_Target
+			fixed4 Frag(V2F i) : SV_Target
 			{
-				i.screenPos /= i.screenPos.w;
-				fixed4 col = tex2D(_MainTex, float2(i.screenPos.x, i.screenPos.y));
-				
+				fixed4 col = tex2D(_MainTex, float2(i.vertex.x / _ScreenParams.x, i.vertex.y / _ScreenParams.y));
 				return col;
 			}
 			ENDCG

f8e360c5e9d3afc1bf9918cdc2a864a60a575470
Kononov Pavel <Hota6blch@gmail.com>
2019-05-27 08:10:20 +0400

    #212 Fix bug
    - Files bugs

---
+++ Assets/Scripts/BackButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //if running on Android, check for Menu/Home and exit
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            if (Input.GetKey(KeyCode.Home) || Input.GetKey(KeyCode.Escape) || Input.GetKey(KeyCode.Menu))
+            {
+                Application.Quit();
+                return;
+            }
+        }
+
+    }
+}
+

53f007c57b123003183e0a4be0628fab5e7ced44
Peter Vasilyev <p.vasilyev@inbox.ru>
2019-06-03 00:40:30 +0300

    #252 Add menu sound button textures

--- Assets/Scripts/Menu/SoundInteractive.cs
+++ Assets/Scripts/Menu/SoundInteractive.cs
@@ -2,8 +2,17 @@
 
 public class SoundInteractive : IInteractiveObject
 {
+    public Texture2D soundOnTexture;
+    public Texture2D soundOffTexture;
+
+    private bool soundOn = true;
+
     public override void Interact()
     {
         AudioListener.volume = 1 - AudioListener.volume;
+
+        soundOn = !soundOn;
+
+        GetComponent<Renderer>().material.SetTexture("_MainTex", soundOn ? soundOnTexture : soundOffTexture);
     }
 }

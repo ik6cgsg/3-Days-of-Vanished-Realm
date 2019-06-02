87997879fe1f21dcd07d5228124e3756fc2c89f7
ik6cgsg <kozlovilya@bk.ru>
2019-06-02 15:40:25 +0300

    #247 Connect sound with firework particle system

---
+++ Assets/Scripts/Firework/RocketController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketController : MonoBehaviour
+{
+    public AudioSource onBirthSound;
+    public AudioSource onDeathSound;
+
+    private int numberOfParticles = 0;
+    private ParticleSystem particleSystem;
+
+    void Start()
+    {
+        particleSystem = GetComponent<ParticleSystem>();    
+    }
+
+    void Update()
+    {
+        if (!onBirthSound && !onDeathSound)
+        {
+            return;
+        }
+        int count = particleSystem.particleCount;
+        if (count < numberOfParticles)
+        {
+            onDeathSound.Play();
+        }
+        else if (count > numberOfParticles)
+        {
+            onBirthSound.Play();
+        }
+        numberOfParticles = count;
+    }
+}
